676dadd09f4662948c8278f28be6da04fdc0be6e
hyyperlite <hyyperlite@gmail.com>
2024-01-31 09:39:46 -0500

    added option to include password in csv output file.

--- ConvertEvokeEmailToCSV.py
+++ ConvertEvokeEmailToCSV.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
+"""
+Create a CSV from the VM instance information in email sent from evoke when new demo has been fully instantiated.
+This is implemented for use with SecureCRT in mind.  It can be used in combination with either (a modified version
+of ImportArbitraryDataToSecureCRT script, available online at VanDyke Support site) -or- (in SecureCRT 9.2+
+the "Import Settings From Text File..." menu option).   The "Import Settings" option allows to set a password for
+each session, while there is no way to do that with import session info script (last i checked).  However, the
+import settings option does require some interaction to set the field order for import each time.
+
+If using Import script then must not include the password field in the csv file (use --no_passwd option in this case)
+"""
 
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", '--outputfile', required=True, help='Path to CSV output file')
 parser.add_argument('-f', '--folder', default='Evoke', help='Name of folder to create sessions in, in SecureCRT')
-parser.add_argument('-c', '--categories', action='store_true', help='Flag whether or not to create folders for diff type of sessions')
+parser.add_argument('-p', '--no_passwd', action='store_true', help="Use as flag to disable output \
+                                         including password field in csv file output")
 args = parser.parse_args()
 
 ##############################################
@@ -18,7 +29,6 @@ if __name__ == '__main__':
     # Currently only relevant for SD-WAN deployments following naming conventions
     # that I (Nick Petersen) typically use.
     print(args)
-    do_folder_categories = args.categories
 
     output_file = args.outputfile
     default_scrt_folder = args.folder
@@ -26,14 +36,18 @@ if __name__ == '__main__':
     emulation = 'ANSI'
 
     outfile = open(output_file, 'w')
-    outfile.write('folder,session_name,hostname,username,protocol,emulation\n')
+    if args.no_passwd:
+        outfile.write('folder,session_name,hostname,username,protocol,emulation\n')
+    else:
+        outfile.write('folder,session_name,hostname,username,password,protocol,emulation\n')
+        
 
     email_content = []
     line_content = []
     template_name = input("Enter name of Evoke template: ")
     scrt_folder = f'{default_scrt_folder}/{template_name}'
 
-    # Get multi-line input from user, using ctrl+d to indicate end of input
+    # Get multi-line input from user, using \n!!!! to indicate end of input
     print('#--> Paste evoke email instance data from email. (then single line with "!!!!" to submit) <--# ')
     while True:
         try:
@@ -43,8 +57,6 @@ if __name__ == '__main__':
         except EOFError:
             break
         email_content.append(line)
-
-    # print(email_content)
 
     for line in email_content:
         # line = line.lstrip('o').lstrip()
@@ -61,21 +73,11 @@ if __name__ == '__main__':
         print(f'password: {password}')
         print('######')
 
-        if do_folder_categories:
-            if 'fg-dc' in hostname:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/headends'
-            elif 'fg-branch' in hostname:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/branches'
-            elif 'ubu' in hostname:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/ubuntu'
-            elif ('fmg'  or 'fac') in hostname:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/misc'
-            elif ('fg-pe' or 'fg-core') in hostname:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/routers'
-            else:
-                scrt_folder = f'{default_scrt_folder}/{template_name}/misc'
-
-        outfile.write(f'{scrt_folder},{hostname},{ext_ip},{username},{protocol},{emulation}\n')
+        if args.no_passwd:
+            outfile.write(f'{scrt_folder},{hostname},{ext_ip},{username},{protocol},{emulation}\n')
+        else:
+            outfile.write(f'{scrt_folder},{hostname},{ext_ip},{username},{password},{protocol},{emulation}\n')
+            
     outfile.close()
     print(f'CSV file written at: {args.outputfile}')
     
